ef30193a1c8790bc98deb884565c1f802dd486e1
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2020-08-15 15:14:44 +0100

    data validations using mongoose validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,29 @@
 const User = require('../models/User');
 
+// Handling errors
+const handleErrors = (err) => {
+    //console.log(err.message, err.code);
+    let errors = { email: '', password: ''};
+
+    // Duplicate error code
+    if(err.code === 11000) {
+        errors.email = 'That email has already registered';
+        return errors;
+    }
+
+    // Validation errors
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties})=>{
+            console.log(properties.message);
+            errors[properties.path] = properties.message;
+        })
+    }
+
+    return errors;
+}
+
+// Creating Paths
+
 module.exports.signup_get = (req, res)=>{
     res.render('signup');
 }
@@ -20,8 +44,8 @@ module.exports.signup_post = async (req, res)=>{
         res.status(201).json(user);
 
     } catch(err){
-        console.log(err);
-        res.status(400).send('error, user not created');
+        const error = handleErrors(err);
+        res.status(400).json(error);
     }
 }
 
